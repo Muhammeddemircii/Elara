97e3c88a4712c524f723b9bbe53b99e640de654e
Muhammed demirci <82472683+Muhammeddemircii@users.noreply.github.com>
2025-04-10 17:18:19 +0300

    Update blogs-news.js

--- src/app/components/blogs-news.js
+++ src/app/components/blogs-news.js
@@ -7,10 +7,10 @@ import { useTranslation } from 'react-i18next';
 import { BlogsNewsdata } from '../Data/data';
 import * as Unicons from '@iconscout/react-unicons';
 
-export default function BlogsNews({title,desc}) {
+export default function BlogsNews({ title, desc }) {
     const { t } = useTranslation();
     const data = BlogsNewsdata.slice(0, 3)
-    return ( 
+    return (
         <>
             <div id="blogs-news" className="container lg:py-24 py-16 bg-white">
                 <div className="grid grid-cols-1 pb-8 text-center">
@@ -22,15 +22,22 @@ export default function BlogsNews({title,desc}) {
                 <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-[30px] mt-8">
                     {data.map((item, index) => (
                         <div className="blog relative rounded-md shadow shadow-slate-200 dark:shadow-gray-800 overflow-hidden" key={index}>
-                            <Image src={item.image} alt="" width={0} height={0} sizes='100vw' style={{width:"100%", height:"auto"}} />
+                            <Image src={item.image} alt="" width={0} height={0} sizes='100vw' style={{ width: "100%", height: "auto" }} />
 
                             <div className="content p-6">
                                 <Link href={`blog-detail/${item.id}`} className="text-lg hover:text-[#7F206D] dark:text-white dark:hover:text-[#7F206D] transition duration-500 ease-in-out font-medium h5 font-montserrat">{item.title}</Link>
                                 <p className="text-[#969696] mt-3 font-montserrat">{item.detail}</p>
 
                                 <div className="mt-5">
-                                    <Link href={`https://wa.me/905497356406`} target="_blank" className="hover:text-[#7F206D] dark:hover:text-[#7F206D] dark:text-white transition duration-500 inline-flex items-center gap-1 font-montserrat">{t('buyNow')}<Unicons.UilArrowRight width={16}/></Link>
+                                    <Link
+                                        href={`https://wa.me/905497356406?text=${encodeURIComponent(`Merhaba, "${item.title}" adlı ürün hakkında bilgi almak istiyorum.`)}`}
+                                        target="_blank"
+                                        className="hover:text-[#7F206D] dark:hover:text-[#7F206D] dark:text-white transition duration-500 inline-flex items-center gap-1 font-montserrat"
+                                    >
+                                        {t('buyNow')}<Unicons.UilArrowRight width={16} />
+                                    </Link>
                                 </div>
+
                             </div>
                         </div>
                     ))}
